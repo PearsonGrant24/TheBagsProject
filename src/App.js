694e32f29f6078ae3add931b66bb471cc3a14cc0
Pearson Grant <pearsongrant23@gmail.com>
2024-11-28 15:25:37 +0200

    all

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { Footer } from './app/components/Footer.tsx';
+import { Footer2 } from './app/components/Footer2.tsx';
+import { Header } from './app/components/Header.tsx';
+import RoutePath from './app/RoutePath.tsx';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <h1>Hello Pearson Grant !!!!!!</h1>
+    // </div>
+
+    <>
+      <Header>
+        <>
+        </>
+      </Header>
+
+      <RoutePath />
+      <Footer>
+        <>
+        </>
+      </Footer>
+      {/* <Footer2>
+        <>
+        </>
+      </Footer2> */}
+      
+    </>
+  );
+}
+
+export default App;
